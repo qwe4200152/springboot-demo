6029465268b29395d13ac2cd8c1aecf65e25fb7c
dean.lee <raines.lee@aliyun.com>
2019-04-29 16:01:03 +0800

    Initial commit

---
+++ startweb-1/src/main/java/demo/springboot/startweb1/Startweb1Application.java
@@ -0,0 +1,13 @@
+package demo.springboot.startweb1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Startweb1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Startweb1Application.class, args);
+    }
+
+}
---
+++ startweb-1/src/main/java/demo/springboot/startweb1/controller/TestController.java
@@ -0,0 +1,13 @@
+package demo.springboot.startweb1.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String test(){
+        return "hello";
+    }
+}
