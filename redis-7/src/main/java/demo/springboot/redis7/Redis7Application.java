b731469fb96c99b9d650ed14cca2a97c59707417
dean.lee <raines.lee@aliyun.com>
2019-05-08 17:14:31 +0800

    redis

---
+++ redis-7/src/main/java/demo/springboot/redis7/Redis7Application.java
@@ -0,0 +1,13 @@
+package demo.springboot.redis7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Redis7Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Redis7Application.class, args);
+    }
+
+}
