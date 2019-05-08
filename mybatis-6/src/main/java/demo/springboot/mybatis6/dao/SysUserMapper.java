cc53d604fc63ae4f049ea9f625d9a8fbd20304c8
dean.lee <raines.lee@aliyun.com>
2019-05-08 16:38:28 +0800

    事务

--- mybatis-6/src/main/java/demo/springboot/mybatis6/dao/SysUserMapper.java
+++ mybatis-6/src/main/java/demo/springboot/mybatis6/dao/SysUserMapper.java
@@ -1,10 +1,15 @@
 package demo.springboot.mybatis6.dao;
 
 import demo.springboot.mybatis6.domain.SysUser;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Update;
 
 /**
  * @author dean.lee
  */
 public interface SysUserMapper {
     SysUser selectById(long id);
+
+    @Update("update sys_user set age = #{age} where id = #{id}")
+    int updateAgeById(@Param("age") int age, @Param("id") long id);
 }
--- mybatis-6/src/test/java/demo/springboot/mybatis6/Mybatis6ApplicationTests.java
+++ mybatis-6/src/test/java/demo/springboot/mybatis6/Mybatis6ApplicationTests.java
@@ -2,6 +2,7 @@ package demo.springboot.mybatis6;
 
 import demo.springboot.mybatis6.dao.SysUserMapper;
 import demo.springboot.mybatis6.domain.SysUser;
+import demo.springboot.mybatis6.service.UserService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,10 +16,26 @@ public class Mybatis6ApplicationTests {
     @Autowired
     private SysUserMapper userMapper;
 
+    @Autowired
+    private UserService userService;
+
     @Test
     public void contextLoads() {
         SysUser sysUser = userMapper.selectById(1L);
         System.out.println(sysUser);
+    }
+
+    @Test
+    public void testTransactional(){
+        System.out.println(userMapper.selectById(1L));
+
+        try {
+            userService.updateUserAge();
+        }catch (RuntimeException e){
+
+        }
+
+        System.out.println(userMapper.selectById(1L));
     }
 
 }
