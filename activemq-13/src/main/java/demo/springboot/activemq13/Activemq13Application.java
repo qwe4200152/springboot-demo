3015e32140b0e0064aed732d78579b0ca57c8d39
dean.lee <raines.lee@aliyun.com>
2019-05-14 20:01:58 +0800

    activeMQ

---
+++ activemq-13/src/main/java/demo/springboot/activemq13/Activemq13Application.java
@@ -0,0 +1,13 @@
+package demo.springboot.activemq13;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Activemq13Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Activemq13Application.class, args);
+    }
+
+}
