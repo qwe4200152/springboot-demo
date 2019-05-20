246c69b7fcb972d566680b15bc20a6eedaeb0eba
dean.lee <raines.lee@aliyun.com>
2019-05-20 12:32:22 +0800

    redis-session

---
+++ redis-session-17/src/main/java/demo/springboot/redissession17/RedisSession17Application.java
@@ -0,0 +1,13 @@
+package demo.springboot.redissession17;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisSession17Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisSession17Application.class, args);
+    }
+
+}
